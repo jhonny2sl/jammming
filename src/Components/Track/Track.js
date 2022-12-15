cbc7deb79cea0734a8ca942f90b34a4011a3106e
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-15 11:35:43 +0700

    Create + Add JSX Elements to render() in Track.js + Add renderAction() Method + Export <Track />

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Track extends React.Component {
+/*     renderAction() {
+        if (isRemoval) {
+            return <button className="Track-action">-</button>;
+        } else {
+            return <button className="Track-action">+</button>;
+        }
+    }
+
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3><!-- track name will go here --></h3>
+                    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+                </div>
+                this.renderAction();
+            </div>
+        );
+    } */
+}
+
+export default Track;
