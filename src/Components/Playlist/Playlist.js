0fc462691c64733d437181152a0da89396fc7a14
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-16 10:27:02 +0700

    Pass updatePlaylistName.bind(this) to <input />'s onChange listener

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,6 +3,11 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
     handleNameChange(event) {
         this.props.onNameChange(event.target.value);
     }
@@ -10,7 +15,7 @@ class Playlist extends React.Component {
     render() {
         return (
             <div className="Playlist">
-                <input defaultValue={"New Playlist"} />
+                <input defaultValue={"New Playlist"} onChange={this.handleNameChange} />
                 <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
