0aea4a9d738479d4cfe7da3883267a14d987318f
jhonny2sl <jhonnytwosl@gmail.com>
2022-12-21 10:44:48 +0700

    Remove bound this.renderAction & call the function + Fill the value of the hard-coded array's props

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,7 +4,6 @@ import './Track.css';
 class Track extends React.Component {
     constructor(props) { 
         super(props);
-        this.renderAction = this.renderAction.bind(this);
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
     }
@@ -32,7 +31,7 @@ class Track extends React.Component {
                     <h3>{this.props.track.name}</h3>
                     <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
-                {this.renderAction}
+                {this.renderAction()}
             </div>
         );
     }
